8cd6aee574de34818be70fc4e70a983d6fa13957
Niraj Agarwal <niraj.k.agarwal93@gmail.com>
2016-12-15 09:49:05 +0530

    Smaller programs for lab1,2,3,4

--- lab3.tcl
+++ lab3.tcl
@@ -1,98 +1,79 @@
 set ns [new Simulator]
-set tf [open lab.tr w]
-$ns trace-all $tf
-set nf [open lab.nam w]
-$ns namtrace-all $nf
+set nf [open lab.tr w]
+$ns trace-all $nf
+set tf [open lab.nam w]
+$ns namtrace-all $tf
+
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
+
 $n0 label "Source/FTP"
-$n1 label "Source/Telnet"
+$n1 label "Source/TELNET"
 $n3 label "Destination/FTP"
-$n5 label "Desination/Telnet"
-$ns color 1 "red"
-$ns color 2 "orange"
-$ns duplex-link $n0 $n2 100Mb 1ms DropTail
-$ns duplex-link $n1 $n2 100Mb 1ms DropTail
-$ns duplex-link $n2 $n3 100Mb 1ms DropTail
-$ns duplex-link $n2 $n4 100Mb 1ms DropTail
-$ns duplex-link $n4 $n5 100Mb 1ms DropTail
+$n5 label "Destination/TELNET"
+
+$ns duplex-link $n0 $n2 100Mb 300ms DropTail
+$ns duplex-link $n1 $n2 100Mb 300ms DropTail
+$ns duplex-link $n2 $n3 100Mb 300ms DropTail
+$ns duplex-link $n2 $n4 100Mb 300ms DropTail
+$ns duplex-link $n4 $n5 100Mb 300ms DropTail
+
 set tcp0 [new Agent/TCP]
-$ns attach-agent $n0 $tcp0
-set ftp0 [new Application/FTP]
-$ftp0 attach-agent $tcp0
-set sink3 [new Agent/TCPSink]
-$ns attach-agent $n3 $sink3
 set tcp1 [new Agent/TCP]
-$ns attach-agent $n1 $tcp1
-
-
-set telnet1 [new Application/Telnet]
-$telnet1 attach-agent $tcp1
+set sink3 [new Agent/TCPSink]
 set sink5 [new Agent/TCPSink]
+set ftp0 [new Application/FTP]
+set telnet1 [new Application/Telnet]
+$ns attach-agent $n0 $tcp0
+$ns attach-agent $n1 $tcp1
+$ns attach-agent $n3 $sink3
 $ns attach-agent $n5 $sink5
-$telnet1 set packetSize_ 1000Mb
-$telnet1 set interval_ 0.00001
-#The below code is used to connect the tcp agents & sink.
+$ftp0 attach-agent $tcp0
+$telnet1 attach-agent $tcp1
+
 $ns connect $tcp0 $sink3
 $ns connect $tcp1 $sink5
-#The below code is used to give a color to tcp and telnet
-#packets.
-$tcp0 set class_ 1
-$tcp1 set class_ 2
+
+$telnet1 set packetSize_ 1000Mb
+$telnet1 set interval_ 0.0001
+
 proc finish { } {
 global ns nf tf
- exec nam lab.nam &
-close $nf
+$ns flush-trace
+exec nam lab.nam &
 close $tf
-
+close $nf
+set ctr0 0
 set ctr1 0
-set ctr2 0
-
+set thr0 0
 set thr1 0
-set thr2 0
-
 set fid [open lab.tr r]
-
-# 0   1      2 3  4 
-# r 0.101003 0 2 tcp 40 ------- 1 0.0 3.0 0 0
-
-
-while {[gets $fid line] != -1} { 
-
-       if { [string match "*r*" $line] } {
-          #   puts $line
-            set fields [regexp -all -inline {\S+} $line]
-            
-            set c2  [lindex $fields 2]
-            set c3  [lindex $fields 3]
-          #  puts $c2
-          #  puts $c3   
-   #   if($1=="r"&&$3=="2"&&$4=="3")
-            if {    [expr $c2 == 2] &&  [expr $c3 == 3] }  {
-                 set ctr1 [expr $ctr1 + 1 ]
-             }
-  #  if($1=="r"&&$3=="4"&&$4=="5")
-            if {    [expr $c2 == 4] &&  [expr $c3 == 5] }  {
-                 set ctr2 [expr $ctr2 + 1 ]
-             }
-    }
- }
-
-   set thr1 [expr $ctr1 / 5 ] 
-   set thr2 [expr $ctr2 / 5 ] 
-  puts "No of tcp packets  =   $ctr1" 
-  puts "tcp throuput  =   $thr1 packets/sec " 
-  puts "No of telnet packets  =   $ctr2"  
-  puts "telnet throuput  =   $thr2 packets/sec " 
-
-          
+while {[gets $fid line] != -1} {
+	if {[string match "*r*" $line]} {
+		set fields [regexp -all -inline {\S+} $line]
+		set c2 [lindex $fields 2]
+		set c3 [lindex $fields 3]
+		if { [expr $c2==2] && [expr $c3==3] } {
+			set ctr0 [expr $ctr0 + 1]
+		}
+		if { [expr $c2==4] && [expr $c3==5] } {
+			set ctr1 [expr $ctr1 + 1]
+		}		
+	}
+}
+set thr0 [expr $ctr0/5]
+set thr1 [expr $ctr1/5]
+puts "No of packets FTP: $ctr0"
+puts "Throughput FTP: $thr0"
+puts "No of packets TELNET: $ctr1"
+puts "Throughput TELNET: $thr1"
 exit 0
 }
-$ns at 0.1 "$ftp0 start"
-$ns at 0.1 "$telnet1 start"
+$ns at 0.01 "$ftp0 start"
+$ns at 0.01 "$telnet1 start"
 $ns at 5 "finish"
 $ns run
--- lab4.tcl
+++ lab4.tcl
@@ -1,9 +1,9 @@
 set ns [new Simulator]
-set tf [open lab.tr w]
-$ns trace-all $tf
-set nf [open lab.nam w]
-$ns namtrace-all $nf
-$ns color 1 "red"
+set nf [open lab.tr w]
+set tf [open lab.nam w]
+$ns trace-all $nf
+$ns namtrace-all $tf
+
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
@@ -11,82 +11,52 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
-$n1 label "Source/UDP"
-$n3 label "Error Node"
-$n5 label "Destination"
-#The below code is used to create a two Lans (Lan1 and Lan2).
+
 $ns make-lan "$n0 $n1 $n2 $n3" 10Mb 10ms LL Queue/DropTail Mac/802_3
 $ns make-lan "$n4 $n5 $n6" 10Mb 10ms LL Queue/DropTail Mac/802_3
-
-
-#The below code is used to connect node n3 of lan1 and n6 of
-#lan2.
 $ns duplex-link $n3 $n6 100Mb 10ms DropTail
+
 set udp1 [new Agent/UDP]
 $ns attach-agent $n1 $udp1
-set cbr1 [ new Application/Traffic/CBR]
+set cbr1 [new Application/Traffic/CBR]
 $cbr1 attach-agent $udp1
+
 set null5 [new Agent/Null]
 $ns attach-agent $n5 $null5
+
 $ns connect $udp1 $null5
-$cbr1 set packetSize_ 1000
-$cbr1 set interval_ 0.0001 ;# This is the data rate. Change
-# this to increase the rate.
-$udp1 set class_ 1
-# The below code is used to add an error model between the
-#nodes n3 and n6.
+
+$cbr1 set packetSize_ 500Mb
+$cbr1 set interval_ 0.005
+
 set err [new ErrorModel]
 $ns lossmodel $err $n3 $n6
 $err set rate_ 0.2
-# This is the error rate. Change this
-#rate to add errors between n3 and n6.
+
 proc finish { } {
-global nf ns tf
- exec nam lab.nam &
-close $nf
+global ns nf tf
+$ns flush-trace
+exec nam lab.nam &
 close $tf
-
-set ctr1 0
-set ctr2 0
-
-set thr1 0
-set thr2 0
-
+close $nf
+set ctr0 0
+set thr0 0
 set fid [open lab.tr r]
-
-# 0   1      2 3  4 
-# r 0.101003 0 2 tcp 40 ------- 1 0.0 3.0 0 0
-
-
-while {[gets $fid line] != -1} { 
-
-       if { [string match "*r*" $line] } {
-          #   puts $line
-            set fields [regexp -all -inline {\S+} $line]
-            
-            set c2  [lindex $fields 2]
-            set c3  [lindex $fields 3]
-          #  puts $c2
-          #  puts $c3   
-   #   if($1=="r"&&$3=="2"&&$4=="3")
-            if {    [expr $c2 == 8] &&  [expr $c3 == 5] }  {
-                 set ctr1 [expr $ctr1 + 1 ]
-             }
- 
-    }
- }
-
-   set thr1 [expr $ctr1 / 5 ] 
-  
-  puts "No of  packets  =   $ctr1" 
-  puts " throuput  =   $thr1 packets/sec " 
- 
-          
-
-
+while {[gets $fid line] != -1} {
+	if { [string match "*r*" $line] } {
+		set fields [regexp -all -inline {\S+} $line]
+		set c2 [lindex $fields 2]
+		set c3 [lindex $fields 3]
+		if { [expr $c2==8] && [expr $c3==5] } {
+			set ctr0 [expr $ctr0 + 1]
+		}
+	}
+}
+set thr0 [expr $ctr0/5]
+puts "No of packets: $ctr0"
+puts "Throughput: $thr0"
 exit 0
 }
+$ns at 0.01 "$cbr1 start"
 $ns at 5.0 "finish"
-$ns at 0.1 "$cbr1 start"
 $ns run
-
